11c19a8f9144d0bee633367d8285066bcf0990d4
Samriddhi <157903700+READSAM@users.noreply.github.com>
2026-01-17 18:06:15 +0530

    Update engine.py

--- engine.py
+++ engine.py
@@ -26,60 +26,60 @@ class CivicClassifier:
     
             self.headers = {"Authorization": f"Bearer {self.token}"}
     
-        def tag_issue(self, description: str):
-            desc_lower = description.lower()
-    
-            matches = []
-            for _, row in self.keywords_df.iterrows():
-                keyword = str(row['Keyword']).lower()
-                if re.search(r'\b' + re.escape(keyword) + r'\b', desc_lower):
-                    predicted_tag = row['Tag']
-                    matches.append({
-                        "tag": predicted_tag,
-                        "pos": desc_lower.find(keyword)
-                    })
-    
-            if matches:
-                #multiple tags use one which comes first
-                first_match = sorted(matches, key=lambda x: x['pos'])[0]
-                tag = first_match['tag']
-                return {
-                    "tag": tag,
-                    "department": self.tag_to_dept.get(tag, "General Grievances Cell /Public Relations Office"),
-                    "confidence": 1.0,
-                    "method": "keyword_match"
-                }
-            print('moving to AI');
-           #if not found in csb file use model
-            payload = {
-                "inputs": description,
-                "parameters": {"candidate_labels": self.categories},
-                "options": {"wait_for_model": True}
-            }
-            
-            try:
-                response = requests.post(self.api_url, headers=self.headers, json=payload, timeout=10)
-                
-                if response.status_code != 200:
-                    # Handle cases like 503 (Loading) or 401 (Unauthorized)
-                    error_detail = response.json().get("error", "Unknown API Error")
-                    raise Exception(f"HF Router Error {response.status_code}: {error_detail}")
-    
-                result = response.json()
-                
-    
-                top_prediction = result[0]
-                predicted_tag = top_prediction['label']
-                confidence = top_prediction['score']
-    
-                return {
+    def tag_issue(self, description: str):
+        desc_lower = description.lower()
+
+        matches = []
+        for _, row in self.keywords_df.iterrows():
+            keyword = str(row['Keyword']).lower()
+            if re.search(r'\b' + re.escape(keyword) + r'\b', desc_lower):
+                predicted_tag = row['Tag']
+                matches.append({
                     "tag": predicted_tag,
-                    "department": self.tag_to_dept.get(predicted_tag, "General Grievances Cell /Public Relations Office")
-                    "confidence": round(confidence, 2),
-                    "method": "transformer_model"
-                }
-    
-            except Exception as e:
-                # Log the specific error
-                print(f"AI Inference Failure: {e}")
-                raise e
+                    "pos": desc_lower.find(keyword)
+                })
+
+        if matches:
+            #multiple tags use one which comes first
+            first_match = sorted(matches, key=lambda x: x['pos'])[0]
+            tag = first_match['tag']
+            return {
+                "tag": tag,
+                "department": self.tag_to_dept.get(tag, "General Grievances Cell /Public Relations Office"),
+                "confidence": 1.0,
+                "method": "keyword_match"
+            }
+        print('moving to AI');
+       #if not found in csb file use model
+        payload = {
+            "inputs": description,
+            "parameters": {"candidate_labels": self.categories},
+            "options": {"wait_for_model": True}
+        }
+        
+        try:
+            response = requests.post(self.api_url, headers=self.headers, json=payload, timeout=10)
+            
+            if response.status_code != 200:
+                # Handle cases like 503 (Loading) or 401 (Unauthorized)
+                error_detail = response.json().get("error", "Unknown API Error")
+                raise Exception(f"HF Router Error {response.status_code}: {error_detail}")
+
+            result = response.json()
+            
+
+            top_prediction = result[0]
+            predicted_tag = top_prediction['label']
+            confidence = top_prediction['score']
+
+            return {
+                "tag": predicted_tag,
+                "department": self.tag_to_dept.get(predicted_tag, "General Grievances Cell /Public Relations Office")
+                "confidence": round(confidence, 2),
+                "method": "transformer_model"
+            }
+
+        except Exception as e:
+            # Log the specific error
+            print(f"AI Inference Failure: {e}")
+            raise e
