15d6cd87d5db8437229bcbcdd4dcca288cda4185
Samriddhi <157903700+READSAM@users.noreply.github.com>
2026-02-27 15:52:25 +0530

    Refactoring the code

--- ping_script.py
+++ ping_script.py
@@ -1,6 +1,6 @@
-// Source - https://stackoverflow.com/a/79282587
-// Posted by Mama africa, modified by community. See post 'Timeline' for change history
-// Retrieved 2026-02-27, License - CC BY-SA 4.0
+#Source - https://stackoverflow.com/a/79282587
+#Posted by Mama africa, modified by community. See post 'Timeline' for change history
+#Retrieved 2026-02-27, License - CC BY-SA 4.0
 
 import requests
 import schedule
